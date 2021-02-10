a41e05dc17c6c41ada9182583252c4485ec783b7
cjcjon <cjcjon30@gmail.com>
2021-02-11 02:13:42 +0900

    feat: Change koa-bodyparser to koa-body for control form-data
    multipart/form-data를 받기 위해 koa-bodyparser에서 koa-body로 변경

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Koa = require("koa");
 const Router = require("koa-router");
-const bodyParser = require("koa-bodyparser");
+const KoaBody = require("koa-body");
 
 // DB 생성
 require("./db");
@@ -13,11 +13,11 @@ const api = require("./api");
 const app = new Koa();
 const router = new Router();
 
-// 라우터 설정
+// 라우터 기본 경로 설정
 router.use("/api", api.routes());
 
-// 라우터보다 우선으로 bodyparser 적용
-app.use(bodyParser());
+// 라우터보다 우선으로 KoaBody 적용
+app.use(KoaBody({ multipart: true }));
 
 // 라우터 미들웨어 사용
 app.use(router.routes()).use(router.allowedMethods());
