0fe8c1cd4bcbb7e98d41d7b157de2813971176c0
cjcjon <cjcjon30@gmail.com>
2021-02-15 19:40:43 +0900

    feat: Add post image function
    포스트 이미지 추가, 삭제 기능이 추가되었다.
    포스트, 썸네일에 사용되는 코드가 같아서 함수로 따로 뺏음.

--- src/commons/imageUploader.js
+++ src/commons/imageUploader.js
@@ -26,7 +26,7 @@ function makeS3() {
  * @param {File} file 이미지 파일
  * @returns true / false
  */
-exports.isAllowedExtension = (file) => {
+function isAllowedExtension(file) {
   for (let i = 0; i < allowedImageType.length; ++i) {
     if (file.type === allowedImageType[i]) {
       return true;
@@ -34,20 +34,23 @@ exports.isAllowedExtension = (file) => {
   }
 
   return false;
-};
+}
 
 /**
- * 이미지 서버에 이미지 저장
- * @param {File} file 저장할 이미지 파일
+ * 이미지 서버에 업로드
+ * @param {File} file 업로드할 이미지 파일
+ * @param {string} folder 이미지 파일 폴더
+ * @param {string} name 이미지 파일 저장할 이름
  * @returns 이미지 경로
  * @throws {Error} 400 에러
  */
-exports.uploadThumbnail = async (file) => {
+async function uploadImage(file, folder, name) {
   // 에러처리
-  if (file === null || false === this.isAllowedExtension(file)) {
+  if (file === null || false === isAllowedExtension(file)) {
+    console.log("무슨 에러야?");
     let error = new Error("이미지가 유효하지 않습니다");
     error.status = 400;
-    return error;
+    throw error;
   }
 
   const [fileOriginPath, fileOriginName, fileType] = [
@@ -55,7 +58,7 @@ exports.uploadThumbnail = async (file) => {
     file.name,
     file.type,
   ];
-  const saveName = `${process.env.THUMBNAIL_FOLDER}/${uuidv4()}`;
+  const saveName = `${folder}/${name}`;
   const contentType = mime.lookup(fileOriginName);
   const ext = mime.extension(fileType);
 
@@ -69,18 +72,65 @@ exports.uploadThumbnail = async (file) => {
   }).promise();
 
   return `${process.env.END_POINT}/${process.env.BUCKET_NAME}/${saveName}.${ext}`;
-};
+}
 
 /**
- * 썸네일 파일 삭제
- * @param {string} url 썸네일 경로
+ * 이미지 서버에서 삭제
+ * @param {string} folder 이미지가 저장된 폴더
+ * @param {string} url 이미지가 저장된 url
  */
-exports.deleteThumbnail = async (url) => {
-  const fileName = `${process.env.THUMBNAIL_FOLDER}/${path.basename(url)}`;
+async function deleteImage(folder, url) {
+  const fileName = `${folder}/${path.basename(url)}`;
 
   const S3 = makeS3();
   await S3.deleteObject({
     Bucket: process.env.BUCKET_NAME, // 버킷 이름
     Key: fileName, // 삭제할 파일 이름
   }).promise();
+}
+
+/**
+ * 썸네일 이미지 서버에 업로드
+ * @param {File} file 저장할 이미지 파일
+ * @returns 이미지 경로
+ * @throws {Error} 400 에러
+ */
+exports.uploadThumbnail = async (file) => {
+  const saveName = uuidv4();
+  const imageUrl = await uploadImage(
+    file,
+    process.env.THUMBNAIL_FOLDER,
+    saveName,
+  );
+
+  return imageUrl;
+};
+
+/**
+ * 썸네일 이미지 삭제
+ * @param {string} url 썸네일 경로
+ */
+exports.deleteThumbnail = async (url) => {
+  await deleteImage(process.env.THUMBNAIL_FOLDER, url);
+};
+
+/**
+ * 포스트에서 사용하는 이미지 서버에 업로드
+ * @param {File} file 저장할 이미지 파일
+ * @returns 이미지 경로
+ * @throws {Error} 400 에러
+ */
+exports.uploadPostImage = async (file) => {
+  const saveName = uuidv4();
+  const imageUrl = await uploadImage(file, process.env.POST_FOLDER, saveName);
+
+  return imageUrl;
+};
+
+/**
+ * 포스트 이미지 삭제
+ * @param {string} url 포스트 이미지 경로
+ */
+exports.deletePostImage = async (url) => {
+  await deleteImage(process.env.POST_FOLDER, url);
 };
