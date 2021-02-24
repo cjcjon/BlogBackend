44e20f5c82d99e206a6fd6c6ae8bc584904fcbfe
cjcjon <cjcjon30@gmail.com>
2021-02-24 10:17:14 +0900

    feat: Enable html header tag id attribute

--- src/commons/sanitizeHtml.js
+++ src/commons/sanitizeHtml.js
@@ -29,6 +29,12 @@ const sanitizeOption = {
     "br",
   ],
   allowedAttributes: {
+    h1: ["id"],
+    h2: ["id"],
+    h3: ["id"],
+    h4: ["id"],
+    h5: ["id"],
+    h6: ["id"],
     a: ["href", "class", "style", "name", "rel", "target"],
     img: ["src", "class", "style", "width", "height", "data-align"],
     iframe: [
