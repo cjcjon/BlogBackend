9682c6dc7d9b74ab502bfb7157d278c1fb74a5ab
cjcjon <cjcjon30@gmail.com>
2021-02-25 14:12:21 +0900

    feat: Add request client ip attribute
    request-ip 모듈을 활용해서 client ip를 request에 추가

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const Koa = require("koa");
 const Router = require("koa-router");
 const KoaBody = require("koa-body");
+const requestIp = require("request-ip");
 
 // DB 생성
 require("./db");
@@ -15,6 +16,18 @@ const router = new Router();
 
 // 라우터 기본 경로 설정
 router.use("/api", api.routes());
+
+// clientIp request에 추가
+app.use(async (ctx, next) => {
+  const ipAttributeName = "clientIp";
+  const ip = requestIp.getClientIp(ctx.request);
+  Object.defineProperty(ctx.request, ipAttributeName, {
+    get: () => ip,
+    configurable: true,
+  });
+
+  await next();
+});
 
 // 라우터보다 우선으로 KoaBody 적용
 app.use(KoaBody({ multipart: true }));
