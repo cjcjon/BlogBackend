50ce5db9536529222f8cd2401f86b7b1ada34e2f
cjcjon <cjcjon30@gmail.com>
2021-03-14 01:09:57 +0900

    feat: Save day when insert visitor's ip
    아이피 저장할 때 날짜도 같이 저장하게 수정

--- src/db/service/visitor.service.js
+++ src/db/service/visitor.service.js
@@ -3,9 +3,10 @@ const dayjs = require("dayjs");
 
 /**
  * 방문인의 IP를 저장한다
- * @param {string} IP 현재 IP
+ * @param {string} ip 현재 ip
+ * @param {string} day 현재 날짜
  */
-exports.insertIP = async (IP, day) => {
+exports.insertIP = async (ip, day) => {
   let conn = null;
   try {
     conn = await pool.getConnection();
@@ -14,9 +15,10 @@ exports.insertIP = async (IP, day) => {
     await conn.beginTransaction();
 
     // IP 정보 저장
-    await conn.query("INSERT INTO today_visitors(ip) VALUES(INET6_ATON(?))", [
-      IP,
-    ]);
+    await conn.query(
+      "INSERT INTO today_visitors(ip, day) VALUES(INET6_ATON(?), ?)",
+      [ip, day],
+    );
 
     // 방문인 수 변경
     await conn.query(
@@ -35,6 +37,10 @@ exports.insertIP = async (IP, day) => {
   }
 };
 
+/**
+ * 최신 날짜 순으로 날짜별 방문자 가져오기
+ * @param {number} limit 가져올 날짜 갯수
+ */
 exports.select = async (limit) => {
   const res = await pool.query(
     "SELECT day, total FROM total_visitors ORDER BY day DESC LIMIT ?",
